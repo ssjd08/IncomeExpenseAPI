f26238ebdc490a8c52c901206b1d2d3250b11987
sajad porkhande <>
2023-05-07 07:53:05 +0330

    [add]swagger complete (end of session 6)

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, status
-from .serializers import RegisterSerializer
+from rest_framework import generics, status, views
+from .serializers import RegisterSerializer, EmailVerificationSerializer
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User 
@@ -9,6 +9,8 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 import jwt
 from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 # Create your views here.
@@ -35,7 +37,11 @@ class RegisterView(generics.GenericAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class VerifyEmail(generics.GenericAPIView):
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
+    token_param_config = openapi.Parameter('token', in_=openapi.IN_QUERY, description='Description' , type=openapi.TYPE_STRING)
+    
+    @swagger_auto_schema(manual_parameters=[token_param_config])
     def get(self, request):
         token = request.GET.get('token')
         try:
@@ -43,10 +49,13 @@ class VerifyEmail(generics.GenericAPIView):
             user = User.objects.get(id = payload['user_id'])
             if not user.is_verified:
                 user.is_verfied = True
-                user.save()                   
+                user.save()         
+                          
             return Response({'email':'succesfully activated.'}, status=status.HTTP_200_OK)
+        
         except jwt.ExpiredSignatureError as identifier:
             return Response({'error':'Activition Expired!'}, status=status.HTTP_400_BAD_REQUEST)
+        
         except jwt.exceptions.DecodeError as identifier:
             return Response({'error':'invalid token'}, status=status.HTTP_400_BAD_REQUEST)
         
