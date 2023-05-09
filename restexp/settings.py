0f4f5625aceabcb405691256cc376a694d953e6a
sajad porkhande <>
2023-05-09 14:40:35 +0330

    [add]pagination changed to 5 per page.

--- restexp/settings.py
+++ restexp/settings.py
@@ -96,9 +96,12 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     'NON_FIELD_ERRORS_KEY': 'Error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',    )
+    
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
