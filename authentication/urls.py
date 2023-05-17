4841b5c1fce3e1b54cddfa708e71786ffddf98a5
sajad porkhande <>
2023-05-17 19:55:29 +0330

    [add] feature Refreshtoken view added from simplejwt.view library.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail, SetNewPasswordAPIVeiw
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
 
 urlpatterns = [
     path ('register/', RegisterView.as_view(), name='register'),
     path ('email-verify/', VerifyEmail.as_view(), name='verify_email'),
     path ('login/', LoginAPIView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path ('password-resrt/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'), # for pass var too utls we should put var in <>.
     path ('request-resrt-email/', RequestPasswordResetEmail.as_view(), name='request-resrt-email'),
     path ('password-resrt-complete', SetNewPasswordAPIVeiw.as_view(), name='password-resrt-complete'),
