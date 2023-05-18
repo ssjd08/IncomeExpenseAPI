aed7c2fb5824077cbc303d3c2380338e35af9c01
sajad porkhande <>
2023-05-18 20:01:41 +0330

    nothing change we already cheanged that

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -39,7 +39,7 @@ class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=3)
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
     username = serializers.CharField(max_length=68, min_length=6, read_only=True)
-    tokens = serializers.SerializerMethodField()
+    tokens = serializers.SerializerMethodField() #automaticly search for get_tokens method in class.
 
     def get_tokens(self, obj):
         user = User.objects.get(email=obj['email'])
